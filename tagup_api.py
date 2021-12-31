70b10b59cd78f0b48c13a7ec70462c5f033de93c
Scott Crawshaw <scott.crawshaw@gmail.com>
2021-12-31 16:29:25 -0500

    Adjusted POST /data endpoint to reflect goals of challenge, which do not necessitate storing each full measurement json object.

--- tagup_api.py
+++ tagup_api.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+from collections import defaultdict
+
 api = Flask("tagup-api")
-data = []
+measurements = defaultdict(list)
+last_measurement = None
+count = 0
 
 @api.route('/healthz')
 def get_healthz():
@@ -8,8 +12,12 @@ def get_healthz():
 
 @api.route('/data', methods=["POST"])
 def post_data():
-    global data
-    data += request.json
+    global measurements, last_measurement, count
+    for data in request.json:
+        measurements[data["sensor"]].append(data["value"])
+        last_measurement = data["timestamp"]
+        count += 1
+
     return "", 201
 
 api.run()
