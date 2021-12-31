ba92bf79d20a18622a10f7781826a55a175a3029
Scott Crawshaw <scott.crawshaw@gmail.com>
2021-12-31 15:53:31 -0500

    Added POST /data endpoint, which saves json objects to data list (Resolved #2). Minor adjustment to healthz endpoint.

--- tagup_api.py
+++ tagup_api.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 api = Flask("tagup-api")
+data = []
 
-@api.route('/healthz/')
+@api.route('/healthz')
 def get_healthz():
-    return ("", 204)
+    return "", 204
+
+@api.route('/data', methods=["POST"])
+def post_data():
+    global data
+    data += request.json
+    return "", 201
 
 api.run()
