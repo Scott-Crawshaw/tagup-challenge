d7a3952ddf82eba8b753860db45644dba1e0c76f
Scott Crawshaw <scott.crawshaw@gmail.com>
2022-01-01 10:08:24 -0500

    After further review of instructions, I realized that the GET /statistics endpoint needed to return different things than I intially thought. I made changes to reflect that. Also included comments, and added data checking to the POST /data endpoint.

--- tagup_api.py
+++ tagup_api.py
@@ -1,29 +1,54 @@
+# Scott Crawshaw
+# Tagup backend challenge
+
 from flask import Flask, request
 from collections import defaultdict
 
 api = Flask("tagup-api")
-measurements = defaultdict(list)
-last_measurement = None
-count = 0
 
-@api.route('/healthz')
+measurements = defaultdict(list)
+
+# returns 204 if server is running
+@api.route('/healthz', methods = ["GET"])
 def get_healthz():
     return "", 204
 
-@api.route('/data', methods=["POST"])
+# accepts JSON array of measurements and stores relevant information
+@api.route('/data', methods = ["POST"])
 def post_data():
-    global measurements, last_measurement, count
-    for data in request.json:
-        measurements[data["sensor"]].append(data["value"])
-        last_measurement = data["timestamp"]
-        count += 1
+    global measurements
+    try:
+        for data in request.json:
+            assert "timestamp" in data and "value" in data and "sensor" in data
+            sensor_id = data.pop("sensor")
+            measurements[sensor_id].append(data)
+        return "", 204
+    # case where request.json is not a list
+    except TypeError:
+        return "Body of request must be a JSON array. Please check request and try again.", 400
+    # case where 'sensor', 'value', or 'timestamp' keys do not exist
+    except AssertionError:
+        return "All measurements must contain 'sensor', 'value', and 'timestamp' keys. Please check request and try again.", 400
 
-    return "", 201
-
-@api.route('/statistics/<string:id>')
+# returns the following as a JSON object:
+# timestamp of last measurement for sensor ('null' if no measurements)
+# total number of measurements sent to server for sensor
+# average value of measurements for sensor (0 if no measurements)
+@api.route('/statistics/<string:id>', methods = ["GET"])
 def get_stats(id):
-    response = {"last_measurement" : last_measurement, "count" : count}
-    response["avg"] = sum(measurements[id])/(len(measurements[id]) or 1)
+    response = {}
+    response["count"] = len(measurements[id])
+    response["avg"] = sum(data["value"] for data in measurements[id])/(len(measurements[id]) or 1)
+    if response["count"] > 0:
+        response["last_measurement"] = measurements[id][-1]["timestamp"]
+    else:
+        response["last_measurement"] = None
     return response, 200
+
+# deletes measurements for given sensor id
+@api.route('/statistics/<string:id>', methods = ["DELETE"])
+def delete_stats(id):
+    measurements.pop(id, None)
+    return "", 204
 
 api.run()
