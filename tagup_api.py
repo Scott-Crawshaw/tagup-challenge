5e9ce26a84c723ba21338b9b19529260802f24f0
Scott Crawshaw <scott.crawshaw@gmail.com>
2022-01-01 10:18:25 -0500

    Added extra data check in POST /data, and adjusted comments.

--- tagup_api.py
+++ tagup_api.py
@@ -6,29 +6,30 @@ from collections import defaultdict
 
 api = Flask("tagup-api")
 
-measurements = defaultdict(list)
+measurements = defaultdict(list) # using defaultdict helps eliminate sensor initialization code
 
 # returns 204 if server is running
 @api.route('/healthz', methods = ["GET"])
 def get_healthz():
     return "", 204
 
-# accepts JSON array of measurements and stores relevant information
+# accepts JSON array of measurements and stores them in measurements dictionary
 @api.route('/data', methods = ["POST"])
 def post_data():
     global measurements
     try:
         for data in request.json:
-            assert "timestamp" in data and "value" in data and "sensor" in data
+            assert "timestamp" in data and "value" in data and "sensor" in data # ensure all keys are present
+            assert type(data["value"]) in [int, float] # ensure 'value' is numeric
             sensor_id = data.pop("sensor")
             measurements[sensor_id].append(data)
         return "", 204
     # case where request.json is not a list
     except TypeError:
         return "Body of request must be a JSON array. Please check request and try again.", 400
-    # case where 'sensor', 'value', or 'timestamp' keys do not exist
+    # case where 'sensor', 'value', or 'timestamp' keys do not exist, or 'value' is non-numeric
     except AssertionError:
-        return "All measurements must contain 'sensor', 'value', and 'timestamp' keys. Please check request and try again.", 400
+        return "All measurements must contain 'sensor', 'value', and 'timestamp' keys, and 'value' must be numeric. Please check request and try again.", 400
 
 # returns the following as a JSON object:
 # timestamp of last measurement for sensor ('null' if no measurements)
